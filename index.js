0c6eddd77ae73d5c51bb8132f4b711105a2ccdd8
Madison Evans <m.corbinevans@gmail.com>
2023-05-22 10:57:43 -0700

    added a confirmation message

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
 	const form = document.querySelector(".form-container");
-	const pageContainer = document.querySelector(".page-container");
+	const background = document.querySelector(".background");
 
 	form.addEventListener("submit", function (event) {
+		event.preventDefault();
+
 		if (form.checkValidity()) {
-			event.preventDefault();
-			pageContainer.style.backgroundImage = "none";
-			pageContainer.style.backgroundColor = "#90ee90";
+			background.style.backgroundImage = "none";
+			background.style.backgroundColor = "#90ee90";
 			form.reset();
+
+			// Create and append the confirmation message
+			const confirmationMessage = document.createElement("span");
+			confirmationMessage.classList.add("confirmation-message");
+			confirmationMessage.textContent = "Your handle has been reserved!";
+			form.appendChild(confirmationMessage);
+
+			// Remove the confirmation message after a delay (e.g., 3 seconds)
+			setTimeout(function () {
+				confirmationMessage.remove();
+			}, 3000);
 		}
 	});
 });
