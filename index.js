f4bf63448653a85c9cd0d7e10b15a48c6987953b
Madison Evans <m.corbinevans@gmail.com>
2023-05-22 11:59:44 -0700

    style updates and color changes on submit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
 	const form = document.querySelector(".form-container");
 	const background = document.querySelector(".background");
+	const title = document.querySelector(".title");
+	const description = document.querySelector(".description");
 
 	form.addEventListener("submit", function (event) {
 		event.preventDefault();
@@ -10,13 +12,17 @@ document.addEventListener("DOMContentLoaded", function () {
 			background.style.backgroundColor = "#90ee90";
 			form.reset();
 
+			// Update text color to black
+			title.style.color = "black";
+			description.style.color = "black";
+
 			// Create and append the confirmation message
 			const confirmationMessage = document.createElement("span");
 			confirmationMessage.classList.add("confirmation-message");
 			confirmationMessage.textContent = "Your handle has been reserved!";
 			form.appendChild(confirmationMessage);
 
-			// Remove the confirmation message after a delay (e.g., 3 seconds)
+			// Remove the confirmation message after a delay
 			setTimeout(function () {
 				confirmationMessage.remove();
 			}, 3000);
