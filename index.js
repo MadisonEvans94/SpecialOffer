f8ac12100bd68434ed22ce0eefbcb7bbf15a168e
Madison Evans <m.corbinevans@gmail.com>
2023-05-22 00:46:43 -0700

    working form implementation

---
+++ index.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+	const form = document.querySelector(".form-container");
+	const pageContainer = document.querySelector(".page-container");
+
+	form.addEventListener("submit", function (event) {
+		if (form.checkValidity()) {
+			event.preventDefault();
+			pageContainer.style.backgroundImage = "none";
+			pageContainer.style.backgroundColor = "#90ee90";
+		}
+	});
+});
