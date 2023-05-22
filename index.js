38ea6a4ac0c4bee94232fe2933abc209254f6e9d
Madison Evans <m.corbinevans@gmail.com>
2023-05-22 12:20:46 -0700

    style updates

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
 	const form = document.querySelector(".form-container");
 	const background = document.querySelector(".background");
 	const title = document.querySelector(".title");
 	const description = document.querySelector(".description");
 
-	form.addEventListener("submit", function (event) {
+	form.addEventListener("submit", (event) => {
 		event.preventDefault();
 
 		if (form.checkValidity()) {
 			background.style.backgroundImage = "none";
-			background.style.backgroundColor = "#90ee90";
+			background.style.backgroundColor = "var(--blue)";
 			form.reset();
 
-			// Update text color to black
-			title.style.color = "black";
-			description.style.color = "black";
+			title.style.color = "var(--black)";
+			description.style.color = "var(--black)";
 
-			// Create and append the confirmation message
 			const confirmationMessage = document.createElement("span");
 			confirmationMessage.classList.add("confirmation-message");
 			confirmationMessage.textContent = "Your handle has been reserved!";
 			form.appendChild(confirmationMessage);
 
-			// Remove the confirmation message after a delay
-			setTimeout(function () {
+			setTimeout(() => {
 				confirmationMessage.remove();
 			}, 3000);
+		} else {
+			// Display an error message or handle the invalid form input
+			alert("Please fill in all fields correctly");
 		}
 	});
 });
